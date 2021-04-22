167334ecc5ed563ed42bf38f19929a93c3ae6989
Julieta Iotti <68659216+Waffleland@users.noreply.github.com>
2021-04-22 16:34:13 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ $(document).ready(function(){
     captions: true,
     slideWidth: 1200
   });
+
+
+   
+    $("#login form").submit(function(){
+       var codigo = $('#password').val();
+      var recetario = document.getElementById('recetario');
+      if(codigo === 'premioespecial1'){
+          recetario.style.display = "block";
+        }
+        else{
+          alert('Còdigo incorrecto, volvè a intentarlo')
+        }
+        return false;
+  });
 });
 
     
